4cded5a121d45b55283bfe21920c861f16e956e1
syuka <ayaka.katafuchi@rakus-partners.co.jp>
2025-01-20 11:17:56 +0900

    second commit

---
+++ src/Ex05.java
@@ -0,0 +1,20 @@
+public class Ex05 {
+
+    public static void main(String[] args) {
+        
+        String bigNumber = "阿僧祇(あそうぎ):那由多(なゆた):不可思議(ふかしぎ):無量大数(むりょうたいすう)";
+
+        String[] bigNumbers = bigNumber.split(":");
+       
+
+        
+            System.out.println(bigNumber);
+
+         
+         
+       
+
+    
+}
+}
+
